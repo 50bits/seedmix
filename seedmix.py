87ee534c5f63c28e5516c9374edb768191143b84
50bits <85784459+50bits@users.noreply.github.com>
2021-06-13 20:52:36 +0200

    Update seedmix.py

--- seedmix.py
+++ seedmix.py
@@ -1,8 +1,6 @@
 import os
 import random
 import json
-import calendar;
-import time;
 
 base_path = os.path.dirname(__file__)
 system_random = random.SystemRandom()
@@ -11,33 +9,36 @@ shuffled_word_index_map = {}
 index_word_map = {}
 word_index_map = {}
 
-current_time = calendar.timegm(time.gmtime())
 
-def remove_line_breaks(list):
-    size = len(list)
+def remove_line_breaks(str_list):
+    size = len(str_list)
     for i in range(0, size):
-        list[i] = list[i].replace("\n", "")
+        str_list[i] = str_list[i].replace("\n", "")
 
-def secure_shuffle(list):
-    size = len(list)
+
+def secure_shuffle(str_list):
+    size = len(str_list)
     for index1 in range(0, size):
-        value1 = list[index1]
+        value1 = str_list[index1]
         index2 = system_random.randrange(0, size)
-        value2 = list[index2]
+        value2 = str_list[index2]
         # exchange
-        list[index1] = value2
-        list[index2] = value1
+        str_list[index1] = value2
+        str_list[index2] = value1
 
-def create_index_word_map(list):
-    size = len(list)
-    for i in range(0, size):
-        shuffled_word_index_map[list[i]] = i + 1
-        index_word_map[i + 1] = list[i]
 
-def create_word_index_map(list):
-    size = len(list)
+def create_index_word_map(str_list):
+    size = len(str_list)
     for i in range(0, size):
-        word_index_map[list[i]] = shuffled_word_index_map[list[i]]
+        shuffled_word_index_map[str_list[i]] = i + 1
+        index_word_map[i + 1] = str_list[i]
+
+
+def create_word_index_map(str_list):
+    size = len(str_list)
+    for i in range(0, size):
+        word_index_map[str_list[i]] = shuffled_word_index_map[str_list[i]]
+
 
 # load seed words file
 # https://github.com/bitcoin/bips/tree/master/bip-0039
@@ -60,10 +61,10 @@ create_word_index_map(words_orig.copy())
 
 # test result maps
 for i in range(1, 2048):
-    if(word_index_map[index_word_map[i]] != i):
+    if word_index_map[index_word_map[i]] != i:
         print("Test failed for index: " + str(i))
 
 # persist result maps
-with open(base_path + "/english_mixed_" + str(current_time) + ".txt", "w") as out_file:
+with open(base_path + "/english-mixed_" + index_word_map[1] + "-" + index_word_map[2048] + ".txt", "w") as out_file:
     out_file.write(json.dumps(word_index_map) + "\n\n\n")
     out_file.write(json.dumps(index_word_map))
